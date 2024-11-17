0eb1737698de9dca6e79318e0e02c62c5e121acb
meongju0o0 <bl4angel20@khu.ac.kr>
2024-11-17 12:52:05 +0900

    욜로 to 장고 서버 연동 완료

--- yolov5/changedetection.py
+++ yolov5/changedetection.py
@@ -4,15 +4,15 @@ import pathlib
 import requests
 from datetime import datetime
 
-
 class ChangeDetection:
     result_prev = []
     HOST = 'http://127.0.0.1:8000'
     username = 'admin'
-    password = 'password'
-    token = ''
-    title = ''
-    text = ''
+    password = 'apdpfhd3!'
+    token = 'dc2370468c357d774045c432acd5936362161aa7'
+    author = 1
+    title = 'yolo_test'
+    text = 'Lorem reprehenderit aliquip ullamco qui duis nulla ipsum consectetur.'
 
     def __init__(self, names):
         self.result_prev = [0 for _ in range(len(names))]
@@ -29,43 +29,50 @@ class ChangeDetection:
     def add(self, names, detected_current, save_dir, image):
         self.title = ''
         self.text = ''
-        change_flag = 0
-        i = 0
-        
-        while i < len(self.result_prev):
+        change_flag = False
+
+
+        for i in range(len(self.result_prev)):
             if self.result_prev[i] == 0 and detected_current[i] == 1:
-                change_flag = 1
+                change_flag = True
                 self.title = names[i]
-                self.text = names[i] + ','
-            i += 1
+                self.text += f"{names[i]},"
 
         self.result_prev = detected_current[:]
 
-        if change_flag == 1:
+        if change_flag:
             self.send(save_dir, image)
     
     def send(self, save_dir, image):
         now = datetime.now()
-        now.isoformat()
+        today = now.date()
 
-        today = datetime.now()
-        save_path = os.getcwd() / save_dir / 'detected' / str(today.year) / str(today.month) / str(today.day)
-        pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
+        save_path = pathlib.Path(save_dir) / 'detected' / str(today.year) / str(today.month) / str(today.day)
+        save_path.mkdir(parents=True, exist_ok=True)
 
-        full_path = save_path / '{0}-{1}-{2}-{3}.jpg'.foramt(today.hour, today.minute, today.second, today.microsecond)
+        file_name = f"{now.strftime('%H-%M-%S')}-{now.microsecond}.jpg"
+        full_path = save_path / file_name
 
+        # Save resized image
         dst = cv2.resize(image, dsize=(320, 240), interpolation=cv2.INTER_AREA)
-        cv2.inwrite(full_path, dst)
+        cv2.imwrite(str(full_path), dst)
 
-        headers = {'Authorization': 'JWT' + self.token, 'Accept': 'application/json'}
+        headers = {
+            'Authorization': f'JWT {self.token}',
+            'Accept': 'application/json'
+        }
 
         data = {
             'title': self.title,
             'text': self.text,
-            'created_date': now,
-            'published_date': now
+            'author': self.author,
+            'created_date': now.isoformat(),
+            'published_date': now.isoformat()
         }
 
-        file = {'image': open(full_path, 'rb')}
-        res = requests.post(self.HOST + './api_root/Post/', data=data, files=file, headers=headers)
-        print(res)
+        files = {'image': open(full_path, 'rb')}
+        try:
+            res = requests.post(f'{self.HOST}/api_root/Post/', data=data, files=files, headers=headers)
+            print(f"Response: {res.status_code}, {res.text}")
+        except requests.RequestException as e:
+            print(f"Failed to send data: {e}")
--- yolov5/detect.py
+++ yolov5/detect.py
@@ -35,6 +35,7 @@ import platform
 import sys
 from pathlib import Path
 
+import numpy as np
 import torch
 
 FILE = Path(__file__).resolve()
@@ -64,6 +65,8 @@ from utils.general import (
     xyxy2xywh,
 )
 from utils.torch_utils import select_device, smart_inference_mode
+
+from changedetection import ChangeDetection
 
 
 @smart_inference_mode()
@@ -148,176 +151,53 @@ def run(
         run(source='data/videos/example.mp4', weights='yolov5s.pt', conf_thres=0.4, device='0')
         ```
     """
-    source = str(source)
-    save_img = not nosave and not source.endswith(".txt")  # save inference images
-    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
-    is_url = source.lower().startswith(("rtsp://", "rtmp://", "http://", "https://"))
-    webcam = source.isnumeric() or source.endswith(".streams") or (is_url and not is_file)
-    screenshot = source.lower().startswith("screen")
-    if is_url and is_file:
-        source = check_file(source)  # download
+    cd = ChangeDetection(names=["person", "bed"])  # 클래스 이름 설정
+    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)
+    (save_dir / "labels").mkdir(parents=True, exist_ok=True)
 
-    # Directories
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-    (save_dir / "labels" if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-
-    # Load model
     device = select_device(device)
-    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+    model = DetectMultiBackend(weights, device=device, data=data)
     stride, names, pt = model.stride, model.names, model.pt
-    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    imgsz = check_img_size(imgsz, s=stride)
 
-    # Dataloader
-    bs = 1  # batch_size
-    if webcam:
-        view_img = check_imshow(warn=True)
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
-        bs = len(dataset)
-    elif screenshot:
-        dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
-    vid_path, vid_writer = [None] * bs, [None] * bs
+    # source를 문자열로 변환
+    source = str(source)
 
-    # Run inference
-    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
-    seen, windows, dt = 0, [], (Profile(device=device), Profile(device=device), Profile(device=device))
+    # 데이터 로더 설정
+    dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt) if source.isnumeric() else LoadImages(source, img_size=imgsz, stride=stride)
+
     for path, im, im0s, vid_cap, s in dataset:
-        with dt[0]:
-            im = torch.from_numpy(im).to(model.device)
-            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
-            im /= 255  # 0 - 255 to 0.0 - 1.0
-            if len(im.shape) == 3:
-                im = im[None]  # expand for batch dim
-            if model.xml and im.shape[0] > 1:
-                ims = torch.chunk(im, im.shape[0], 0)
+        if isinstance(im0s, list):  # 여러 스트림을 사용하는 경우
+            im0s = im0s[0]
 
-        # Inference
-        with dt[1]:
-            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-            if model.xml and im.shape[0] > 1:
-                pred = None
-                for image in ims:
-                    if pred is None:
-                        pred = model(image, augment=augment, visualize=visualize).unsqueeze(0)
-                    else:
-                        pred = torch.cat((pred, model(image, augment=augment, visualize=visualize).unsqueeze(0)), dim=0)
-                pred = [pred, None]
-            else:
-                pred = model(im, augment=augment, visualize=visualize)
-        # NMS
-        with dt[2]:
-            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        if not isinstance(im0s, np.ndarray):  # NumPy 배열이 아닌 경우 변환
+            try:
+                im0s = np.array(im0s)
+            except Exception as e:
+                print(f"Error converting im0s to NumPy array: {e}")
+                continue
 
-        # Second-stage classifier (optional)
-        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+        im = torch.from_numpy(im).to(device)
+        im = im.half() if model.fp16 else im.float()
+        im /= 255.0
+        if len(im.shape) == 3:
+            im = im[None]
 
-        # Define the path for the CSV file
-        csv_path = save_dir / "predictions.csv"
+        pred = model(im)
+        pred = non_max_suppression(pred, conf_thres, iou_thres, max_det=max_det)
 
-        # Create or append to the CSV file
-        def write_to_csv(image_name, prediction, confidence):
-            """Writes prediction data for an image to a CSV file, appending if the file exists."""
-            data = {"Image Name": image_name, "Prediction": prediction, "Confidence": confidence}
-            with open(csv_path, mode="a", newline="") as f:
-                writer = csv.DictWriter(f, fieldnames=data.keys())
-                if not csv_path.is_file():
-                    writer.writeheader()
-                writer.writerow(data)
-
-        # Process predictions
-        for i, det in enumerate(pred):  # per image
-            seen += 1
-            if webcam:  # batch_size >= 1
-                p, im0, frame = path[i], im0s[i].copy(), dataset.count
-                s += f"{i}: "
-            else:
-                p, im0, frame = path, im0s.copy(), getattr(dataset, "frame", 0)
-
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / "labels" / p.stem) + ("" if dataset.mode == "image" else f"_{frame}")  # im.txt
-            s += "{:g}x{:g} ".format(*im.shape[2:])  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0.copy() if save_crop else im0  # for save_crop
-            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+        detected_classes = [0] * len(names)
+        for det in pred:
             if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+                for *xyxy, conf, cls in det:
+                    detected_classes[int(cls)] = 1
 
-                # Print results
-                for c in det[:, 5].unique():
-                    n = (det[:, 5] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+        print("Detected classes:", detected_classes)
+        cd.add(names, detected_classes, save_dir, im0s)
 
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    c = int(cls)  # integer class
-                    label = names[c] if hide_conf else f"{names[c]}"
-                    confidence = float(conf)
-                    confidence_str = f"{confidence:.2f}"
-
-                    if save_csv:
-                        write_to_csv(p.name, label, confidence_str)
-
-                    if save_txt:  # Write to file
-                        if save_format == 0:
-                            coords = (
-                                (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
-                            )  # normalized xywh
-                        else:
-                            coords = (torch.tensor(xyxy).view(1, 4) / gn).view(-1).tolist()  # xyxy
-                        line = (cls, *coords, conf) if save_conf else (cls, *coords)  # label format
-                        with open(f"{txt_path}.txt", "a") as f:
-                            f.write(("%g " * len(line)).rstrip() % line + "\n")
-
-                    if save_img or save_crop or view_img:  # Add bbox to image
-                        c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f"{names[c]} {conf:.2f}")
-                        annotator.box_label(xyxy, label, color=colors(c, True))
-                    if save_crop:
-                        save_one_box(xyxy, imc, file=save_dir / "crops" / names[c] / f"{p.stem}.jpg", BGR=True)
-
-            # Stream results
-            im0 = annotator.result()
-            if view_img:
-                if platform.system() == "Linux" and p not in windows:
-                    windows.append(p)
-                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
-                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == "image":
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video' or 'stream'
-                    if vid_path[i] != save_path:  # new video
-                        vid_path[i] = save_path
-                        if isinstance(vid_writer[i], cv2.VideoWriter):
-                            vid_writer[i].release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                        save_path = str(Path(save_path).with_suffix(".mp4"))  # force *.mp4 suffix on results videos
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
-                    vid_writer[i].write(im0)
-
-        # Print time (inference-only)
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
-
-    # Print results
-    t = tuple(x.t / seen * 1e3 for x in dt)  # speeds per image
-    LOGGER.info(f"Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}" % t)
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ""
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
-    if update:
-        strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
+        if view_img:
+            cv2.imshow(str(path), im0s)
+            cv2.waitKey(1)
 
 
 def parse_opt():
